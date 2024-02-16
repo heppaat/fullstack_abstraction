eb933f64733347be5495ba8c782f21df475f34a5
heppaat <pali.herrer@gmail.com>
2024-02-16 17:18:10 +0100

    type methods to method (no s)

--- frontend/src/http/index.ts
+++ frontend/src/http/index.ts
@@ -1,22 +1,24 @@
-import { z } from "zod"
+import { z } from "zod";
 // network error
 // client error (4xx)
 // server error (5xx)
 // validation error (2xx)
 
-type Response<Type> = {
-  success: true
-  status: number
-  data: Type
-} | {
-  success: false
-  status: number | null
-}
+type Response<Type> =
+  | {
+      success: true;
+      status: number;
+      data: Type;
+    }
+  | {
+      success: false;
+      status: number | null;
+    };
 
-type Methods = "GET" | "POST" | "PATCH" | "DELETE"
+type Method = "GET" | "POST" | "PATCH" | "DELETE";
 
 export const safeFetch = async <Schema extends z.ZodTypeAny>(
-  method: Methods,
+  method: Method,
   url: string,
   schema: Schema,
   payload?: any
@@ -24,40 +26,46 @@ export const safeFetch = async <Schema extends z.ZodTypeAny>(
   try {
     const response = await fetch(url, {
       method,
-      headers: payload ? {
-        'Content-Type': "application/JSON"
-      } : { },
-      body: payload ? JSON.stringify(payload) : undefined
-    })
+      headers: payload
+        ? {
+            "Content-Type": "application/JSON",
+          }
+        : {},
+      body: payload ? JSON.stringify(payload) : undefined,
+    });
 
     if (response.status >= 500)
-      return { success: false, status: response.status }
-    
+      return { success: false, status: response.status };
+
     if (response.status >= 400)
-      return { success: false, status: response.status }
-    
-    const data = await response.json()
+      return { success: false, status: response.status };
 
-    const result = schema.safeParse(data)
-    if (!result.success)
-      return { success: false, status: response.status }
+    const data = await response.json();
 
-    return { data: result.data, success: true, status: response.status }
+    const result = schema.safeParse(data);
+    if (!result.success) return { success: false, status: response.status };
+
+    return { data: result.data, success: true, status: response.status };
   } catch (error) {
-    return { success: false, status: null }
+    return { success: false, status: null };
   }
-}
+};
 
 const MovieSchema = z.object({
-  id: z.number(), title: z.string(), year: z.number()
-})
+  id: z.number(),
+  title: z.string(),
+  year: z.number(),
+});
 
 export const getMovies = (after: number) =>
-  safeFetch("GET", `http://localhost:3000/api/movies?after=${after}`, MovieSchema.array())
+  safeFetch(
+    "GET",
+    `http://localhost:3000/api/movies?after=${after}`,
+    MovieSchema.array()
+  );
 
-
-export const postMovie = async (movie: { title: string, year: number }) =>
-  safeFetch("POST", `http://localhost:3000/api/movies`, MovieSchema, movie)
+export const postMovie = async (movie: { title: string; year: number }) =>
+  safeFetch("POST", `http://localhost:3000/api/movies`, MovieSchema, movie);
 
 /* export const patchMovie = async (id: number, movie: { title: string, year: number }) =>
   safeFetch("PATCH", `http://localhost:3000/api/movies/${id}`, 3, movie)
